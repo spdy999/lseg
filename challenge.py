8dc0a63c026eee710d4945de10575049629c386a
spdy999 <1a1a1a.262626.0010@gmail.com>
2025-09-15 15:17:41 +0700

    refactor

--- challenge.py
+++ challenge.py
@@ -7,6 +7,8 @@ class Solution:
         key = [c for c in str(key).strip().split('/')]
 
         def dfs(obj: dict, key: List[str]) -> str:
+            if key[0] not in obj:
+                return "Sorry, we can't find the value from the key. Please recheck the key"
             if len(key) == 1:
                 return obj[key[0]]
             key1 = key.pop(0)
@@ -19,16 +21,23 @@ print('Running unit tests...')
 s = Solution()
 assert s.solve({"a": {"b": {"c": "d"}}}, 'a/b/c') == 'd'
 assert s.solve({"x": {"y": {"z": "a"}}}, 'x/y/z') == 'a'
+assert s.solve({"x": {"y": {"z": "a"}}}, 'x/y') == {'z': 'a'}
+assert s.solve({"x": {"y": {"z": "a"}}},
+               'x/y/r') == "Sorry, we can't find the value from the key. Please recheck the key"
+assert s.solve({"x": {"y": {"z": "a"}}},
+               'x/y/z/r') == "Sorry, we can't find the value from the key. Please recheck the key"
 print('All passed')
 print()
 
 # User tests
-print('Example Object: {"a": {"b": {"c": "d"}}}')
-obj = json.loads(input('Please enter Object: '))
-print()
-print('Example Key: a/b/c')
-key = input('Please enter Key: ')
-print()
+while True:
+    print('Example Object: {"a": {"b": {"c": "d"}}}')
+    obj = json.loads(input('Please enter Object: '))
+    print()
+    print('Example Key: a/b/c')
+    key = input('Please enter Key: ')
+    print()
 
-s = Solution()
-print('Value:', s.solve(obj, key))
+    s = Solution()
+    print('Value:', s.solve(obj, key))
+    print()
